1611178a1de55eaf65820b37762fe0a3945d8184
FlorenciaMssy <Florencia>
2020-02-12 23:31:25 -0300

    hay que arreglar comensales

--- comensales.py
+++ comensales.py
@@ -4,16 +4,26 @@ import logging
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
+
+semaforoCocinero = threading.Semaphore(0)
+semaforoComensal = threading.Semaphore(3)
+
 class Cocinero(threading.Thread):
   def __init__(self):
     super().__init__()
     self.name = 'Cocinero'
 
   def run(self):
+    semaforoCocinero.acquire
     global platosDisponibles
-    while (True):
-      logging.info('Reponiendo los platos...')
-      platosDisponibles = 3
+    try:
+      while (True):
+        logging.info('Reponiendo los platos...')
+        platosDisponibles = 3
+    finally:
+        semaforoComensal.release()
+        semaforoCocinero.release()
+
 
 class Comensal(threading.Thread):
   def __init__(self, numero):
@@ -21,9 +31,20 @@ class Comensal(threading.Thread):
     self.name = f'Comensal {numero}'
 
   def run(self):
-    global platosDisponibles
-    platosDisponibles -= 1
-    logging.info(f'¡Qué rico! Quedan {platosDisponibles} platos')
+      global platosDisponibles
+
+      semaforoComensal.acquire()
+      try:
+          if platosDisponibles > 0:
+            platosDisponibles -= 1
+            logging.info(f'¡Qué rico! Quedan {platosDisponibles} platos')
+          else:
+            semaforoComensal.acquire()
+            semaforoCocinero.release()
+            
+      finally:
+          semaforoComensal.release()
+      
 
 platosDisponibles = 3
 
@@ -31,4 +52,3 @@ Cocinero().start()
 
 for i in range(5):
   Comensal(i).start()
-
